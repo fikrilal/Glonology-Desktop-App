94372b8c7cd43e61c153908c5934f5d4aa408d34
ahmadedzam <100184562+ahmadedzam@users.noreply.github.com>
2022-04-25 19:31:15 +0700

    Add files via upload

--- kelompoka3/kelompoka3/src/com/kelompoka3/form/formBarang.java
+++ kelompoka3/kelompoka3/src/com/kelompoka3/form/formBarang.java
@@ -4,24 +4,32 @@
  * and open the template in the editor.
  */
 package com.kelompoka3.form;
-
-import java.sql.Connection;
+import com.kelompoka3.koneksi.koneksi;
+import java.sql.*;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+
 
 /**
  *
  * @author ASUS
  */
 public class formBarang extends javax.swing.JPanel {
-
+    private Connection con;
+    private Statement st;
+    private ResultSet RSBarang;
+    private String sql="";
+    
+    private String idBarang, namaBarang, jenis, warna, hargaJual, hargaBeli, stok;
     /**
      * Creates new form formBarang
      */
     public formBarang() {
         initComponents();
         TampilanData();
+        
     }
-    private void CariData(String Key){
+     private void CariData(String Key){
         DefaultTableModel model = new DefaultTableModel();
         model.addColumn("ID Barang");
         model.addColumn("Nama Barang");
@@ -35,7 +43,7 @@ public class formBarang extends javax.swing.JPanel {
         System.out.println(sql);
         
         try{
-            java.sql.Connection conn = (Connection) koneksiphp.configDB();
+            java.sql.Connection conn = (Connection) koneksi.koneksi();
             java.sql.PreparedStatement stm = conn.prepareStatement(sql);
             java.sql.ResultSet rs = stm.executeQuery();
             while(rs.next()){
@@ -63,7 +71,7 @@ public class formBarang extends javax.swing.JPanel {
         System.out.println(sql);
         
         try{
-            java.sql.Connection conn = (Connection) koneksiphp.configDB();
+            java.sql.Connection conn = (Connection) koneksi.koneksi();
             java.sql.PreparedStatement stm = conn.prepareStatement(sql);
             java.sql.ResultSet rs = stm.executeQuery();
            
@@ -77,7 +85,7 @@ public class formBarang extends javax.swing.JPanel {
             e.getMessage();
         }
     }
-
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -96,7 +104,8 @@ public class formBarang extends javax.swing.JPanel {
         tbh1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         crBrg = new javax.swing.JTextField();
-        refreshBtn = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(255, 255, 255));
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -159,7 +168,7 @@ public class formBarang extends javax.swing.JPanel {
         });
         jScrollPane1.setViewportView(jtable1);
 
-        tbh1.setBackground(new java.awt.Color(0, 102, 51));
+        tbh1.setBackground(new java.awt.Color(0, 102, 102));
         tbh1.setText("Tambah");
         tbh1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -175,50 +184,37 @@ public class formBarang extends javax.swing.JPanel {
             }
         });
 
-        refreshBtn.setBackground(new java.awt.Color(0, 0, 255));
-        refreshBtn.setText("Refresh");
-        refreshBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                refreshBtnActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1015, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(crBrg, javax.swing.GroupLayout.PREFERRED_SIZE, 445, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(tbh1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(hps1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(edit1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(refreshBtn)))
-                .addGap(0, 197, Short.MAX_VALUE))
+                .addGap(5, 5, 5)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(crBrg, javax.swing.GroupLayout.PREFERRED_SIZE, 445, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(tbh1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(hps1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(edit1)
+                .addGap(0, 226, Short.MAX_VALUE))
+            .addComponent(jScrollPane1)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
+                .addGap(27, 27, 27)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(crBrg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1)
+                    .addComponent(crBrg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(tbh1)
                     .addComponent(hps1)
-                    .addComponent(edit1)
-                    .addComponent(refreshBtn))
-                .addGap(47, 47, 47)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 631, Short.MAX_VALUE)
-                .addContainerGap())
+                    .addComponent(edit1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 589, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -227,7 +223,7 @@ public class formBarang extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 255, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -239,14 +235,14 @@ public class formBarang extends javax.swing.JPanel {
 
     private void hps1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hps1ActionPerformed
         // TODO add your handling code here:
-        new PopUpHapus().setVisible(true);
+        new PopUpHapusBrg().setVisible(true);
         //       HapusData();
     }//GEN-LAST:event_hps1ActionPerformed
 
     private void edit1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_edit1ActionPerformed
         // TODO add your handling code here:
         // EditData();
-        new PopUpEdit().setVisible(true);
+        new PopUpEditBrg().setVisible(true);
     }//GEN-LAST:event_edit1ActionPerformed
 
     private void jtable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtable1MouseClicked
@@ -290,7 +286,7 @@ public class formBarang extends javax.swing.JPanel {
     }//GEN-LAST:event_jtable1MouseClicked
 
     private void tbh1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tbh1ActionPerformed
-        new PopUpTambah().setVisible(true);
+        new PopUpTambahBrg().setVisible(true);
         /** if(hrgBrgBeli.getText().equals("")|| hrgJual.getText().equals("")){
             JOptionPane.showMessageDialog(this, "Isi dahulu");
         }else{
@@ -318,12 +314,6 @@ public class formBarang extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_crBrgKeyReleased
 
-    private void refreshBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_refreshBtnActionPerformed
-        // TODO add your handling code here:
-        this.setVisible(false);
-        new formBarang().setVisible(true);
-    }//GEN-LAST:event_refreshBtnActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField crBrg;
@@ -333,7 +323,6 @@ public class formBarang extends javax.swing.JPanel {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jtable1;
-    private javax.swing.JButton refreshBtn;
     private javax.swing.JButton tbh1;
     // End of variables declaration//GEN-END:variables
 }
